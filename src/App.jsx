59d86eb149cbc0861e1dc3a3b93898ce69fd70c6
mattlaszcz <matt@hearthdisplay.com>
2025-07-08 19:57:09 -0600

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selection1, setSelection1] = useState();
+  const [selection2, setSelection2] = useState();
+
+  const dummyData = [{name: "Pikachu"},{name: "Pikachu"},{name: "Pikachu"},{name: "Pikachu"},{name: "Pikachu"},{name: "Pikachu"},{name: "Pikachu"},{name: "Pikachu"},{name: "Pikachu"},{name: "Pikachu"}]
+
+  const isSelected = (item) => {
+    item.className
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={"container"}>
+      {
+        dummyData.map((pokemon, index)=>{
+          return(
+            <button onClick={isSelected}  index={index}>
+              <div className={"item"}>
+                {pokemon.name}
+              </div>
+            </button>
+          )
+        })
+      }
+      
+    </div>
   )
 }
 
